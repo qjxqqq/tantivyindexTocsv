0e7c2f03855c921487329123fb9114aebc46c478
qujiaxin <3143375592@qq.com>
2023-06-13 11:34:30 +0800

    v0.1.4

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use tantivy::TantivyError;
 use std::io;
 use anyhow::anyhow;
 use tantivy::schema::Value;
+use regex::Regex;
 
 fn main() -> anyhow::Result<()> {
     // 打开索引文件
@@ -17,7 +18,7 @@ fn main() -> anyhow::Result<()> {
 
     // 获取CSV文件句柄
     let csv_file = File::create("output.csv")?;
-    let mut csv_writer = WriterBuilder::new().delimiter(b'\x01').has_headers(true).from_writer(csv_file);
+    let mut csv_writer = WriterBuilder::new().delimiter(b'\x07').has_headers(true).from_writer(csv_file);
 
     // 获取所有的doc
     let searcher = index.reader()?.searcher();
@@ -38,6 +39,9 @@ fn main() -> anyhow::Result<()> {
     header_record.push("isbn");
     header_record.push("ipfs_cid");
     csv_writer.write_record(header_record)?;
+
+    // 正则表达式匹配不可见字符
+    let regex = Regex::new(r"[\p{Z}\p{C}]").unwrap(); // 匹配空格和不可见字符
 
     // 遍历所有的doc，将数据写入CSV文件
     for (_score, doc_address) in top_docs {
@@ -60,17 +64,41 @@ fn main() -> anyhow::Result<()> {
         let isbn: String = doc.get_first(schema.get_field("isbn").unwrap()).unwrap().as_text().unwrap().to_string();
         let ipfs_cid: String = doc.get_first(schema.get_field("ipfs_cid").unwrap()).unwrap().as_text().unwrap().to_string();
 
+        // 将`\011`替换为空格
+        let cleaned_title = title.replace("\011", " ");
+        let cleaned_author = author.replace("\011", " ");
+        let cleaned_publisher = publisher.replace("\011", " ");
+        let cleaned_extension = extension.replace("\011", " ");
+        let cleaned_filesize = filesize.replace("\011", " ");
+        let cleaned_language = language.replace("\011", " ");
+        let cleaned_year = year.replace("\011", " ");
+        let cleaned_pages = pages.replace("\011", " ");
+        let cleaned_isbn = isbn.replace("\011", " ");
+        let cleaned_ipfs_cid = ipfs_cid.replace("\011", " ");
+
+        // 使用正则表达式替换不可见字符
+        let cleaned_title = regex.replace_all(&cleaned_title, "");
+        let cleaned_author = regex.replace_all(&cleaned_author, "");
+        let cleaned_publisher = regex.replace_all(&cleaned_publisher, "");
+        let cleaned_extension = regex.replace_all(&cleaned_extension, "");
+        let cleaned_filesize = regex.replace_all(&cleaned_filesize, "");
+        let cleaned_language = regex.replace_all(&cleaned_language, "");
+        let cleaned_year = regex.replace_all(&cleaned_year, "");
+        let cleaned_pages = regex.replace_all(&cleaned_pages, "");
+        let cleaned_isbn = regex.replace_all(&cleaned_isbn, "");
+        let cleaned_ipfs_cid = regex.replace_all(&cleaned_ipfs_cid, "");
+
         // 将字段写入CSV记录中
-        csv_record.push(title.replace("\n", " "));
-        csv_record.push(author.replace("\n", " "));
-        csv_record.push(publisher.replace("\n", " "));
-        csv_record.push(extension.replace("\n", " "));
-        csv_record.push(filesize.replace("\n", " "));
-        csv_record.push(language.replace("\n", " "));
-        csv_record.push(year.replace("\n", " "));
-        csv_record.push(pages.replace("\n", " "));
-        csv_record.push(isbn.replace("\n", " "));
-        csv_record.push(ipfs_cid.replace("\n", " "));
+        csv_record.push(cleaned_title.to_string());
+        csv_record.push(cleaned_author.to_string());
+        csv_record.push(cleaned_publisher.to_string());
+        csv_record.push(cleaned_extension.to_string());
+        csv_record.push(cleaned_filesize.to_string());
+        csv_record.push(cleaned_language.to_string());
+        csv_record.push(cleaned_year.to_string());
+        csv_record.push(cleaned_pages.to_string());
+        csv_record.push(cleaned_isbn.to_string());
+        csv_record.push(cleaned_ipfs_cid.to_string());
 
         // 将CSV记录写入CSV文件
         csv_writer.write_record(csv_record)?;
